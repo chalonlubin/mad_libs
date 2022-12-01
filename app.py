3f4738faeedcec68a65052523eb95040fbbf0103
Chalon Lubin <chalonlubin1@gmail.com>
2022-11-30 21:23:29 -0800

    final commit

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/questions")
+def show_question_form():
+    prompts = silly_story.prompts
+    return render_template("questions.html", questions=prompts)
+
+@app.get("/results")
+def view_results():
+    answers = request.args
+    story = silly_story.generate(answers)
+    return render_template("results.html", story=story)
+
+
